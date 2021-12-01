1ca1b7ae60608d684ab29c332ea387caafb71152
Hyeon1445 <lily1445@gmail.com>
2021-12-01 22:00:18 +0900

    add gltf file by using GLTFLoader

--- 01/js/myThree.js
+++ 01/js/myThree.js
@@ -11,19 +11,26 @@ scene.add( light )
 const controls = new THREE.OrbitControls( camera, renderer.domElement )
 
 
-const mtlLoader = new THREE.MTLLoader()
-mtlLoader.setPath("assets/")
-mtlLoader.load("StrawberryDonut.mtl", (material) => {
-  material.preload()
-  const objLoader = new THREE.OBJLoader()
-  objLoader.setMaterials(material)
-  objLoader.setPath("assets/")
-  objLoader.load("StrawberryDonut.obj", (object) => {
-    scene.add(object)
-    object.position.y -= 0.1
-  })
-})
+// const mtlLoader = new THREE.MTLLoader()
+// mtlLoader.setPath("assets/")
+// mtlLoader.load("StrawberryDonut.mtl", (material) => {
+//   material.preload()
+//   const objLoader = new THREE.OBJLoader()
+//   objLoader.setMaterials(material)
+//   objLoader.setPath("assets/")
+//   objLoader.load("StrawberryDonut.obj", (object) => {
+//     scene.add(object)
+//     object.position.y -= 0.1
+//   })
+// })
 
+const loader = new THREE.GLTFLoader()
+loader.load(
+  'assets/StrawberryDonut3.glb',
+  (gltf) => {
+    scene.add(gltf.scene)
+  }
+)
 
 camera.position.z = 0.5
 
