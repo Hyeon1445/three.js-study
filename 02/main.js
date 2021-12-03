2cf7d79d4f2c4b66535414ccd74d06433a7b7efe
Hyeon1445 <lily1445@gmail.com>
2021-12-03 09:05:06 +0900

    Directional Light and Shadows

---
+++ 02/main.js
@@ -0,0 +1,51 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.135.0'
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.135.0/examples/jsm/controls/OrbitControls.js'
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const fov = 60 // field of view
+const aspect = 1920 / 1080 // aspect ratio
+const near = 1
+const far = 1000
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+camera.position.set(75, 20, 0)
+
+const scene = new THREE.Scene()
+let light = new THREE.DirectionalLight(0xffffff, 1.0)
+light.position.set(20, 100, 10)
+light.castShadow = true
+scene.add(light)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+let geometry = new THREE.PlaneGeometry(100, 100, 10, 10)
+let material = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+})
+
+const plane = new THREE.Mesh(geometry, material)
+plane.castShadow = true
+plane.receiveShadow = true
+plane.rotation.x = -Math.PI / 2
+scene.add(plane)
+
+const box = new THREE.Mesh(
+  new THREE.BoxGeometry(2, 2, 2),
+  new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+  })
+)
+box.position.set(0, 5, 0)
+box.castShadow = true
+box.receiveShadow = true
+scene.add(box)
+
+const animate = () => {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}
+
+animate()
